184e34d01822f00e1fe23efc85627fe5be1494ab
bingle <415473422@qq.com>
2020-03-19 10:17:16 +0800

    添加一行代码测试

--- FirstDemoTest/FirstDemoTest/Program.cs
+++ FirstDemoTest/FirstDemoTest/Program.cs
@@ -8,6 +8,7 @@ namespace FirstDemoTest
         {
             Console.WriteLine("test");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("添加一行代码");
             Console.ReadLine();
         }
     }
