f77c97e68a44f459a73c343049a917b5f29a5421
bingle <415473422@qq.com>
2020-03-19 10:14:11 +0800

    测试

---
+++ FirstDemoTest/FirstDemoTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstDemoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
